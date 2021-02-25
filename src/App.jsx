3e45cd9e5ed616a5bceb1b1941b2a48fb0ae6c66
Clare Forrest <clare.a.forrest@gmail.com>
2021-02-25 15:50:23 +1100

    remove drag functionality from main app and place in own file as function

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,30 @@
 import './App.css';
+import { drag } from './components/dragFunction';
+import firefighter_hat from './images/firefighter_hat.jpg'
+import firefighter_pants from './images/firefighter_pants.png'
+import firefighter_boots from './images/firefighter_boots.jpg'
+import firefighter_jacket from './images/firefighter_jacket.png'
 
 function App() {
-  let dragged;
-
-/* events fired on the draggable target */
-document.addEventListener("drag", function(event) {
-
-}, false);
-
-document.addEventListener("dragstart", function(event) {
-  // store a ref. on the dragged elem
-  dragged = event.target;
-  // make it half transparent
-  event.target.style.opacity = .5;
-}, false);
-
-document.addEventListener("dragend", function(event) {
-  // reset the transparency
-  event.target.style.opacity = "";
-}, false);
-
-/* events fired on the drop targets */
-document.addEventListener("dragover", function(event) {
-  // prevent default to allow drop
-  event.preventDefault();
-}, false);
-
-document.addEventListener("dragenter", function(event) {
-  // highlight potential drop target when the draggable element enters it
-  if (event.target.className == "dropzone") {
-    event.target.style.background = "purple";
-  }
-
-}, false);
-
-document.addEventListener("dragleave", function(event) {
-  // reset background of potential drop target when the draggable element leaves it
-  if (event.target.className == "dropzone") {
-    event.target.style.background = "";
-  }
-
-}, false);
-
-document.addEventListener("drop", function(event) {
-  // prevent default action (open as link for some elements)
-  event.preventDefault();
-  // move dragged elem to the selected drop target
-  if (event.target.className == "dropzone") {
-    event.target.style.background = "";
-    dragged.parentNode.removeChild( dragged );
-    event.target.appendChild( dragged );
-  }
-}, false);
-  return (
-    <div> 
-      <h1>Get the Fire Fighter Ready!</h1>
-      <div>
-        <div id="helmet-draggable" draggable="true" ondragstart="event.dataTransfer.setData('text/plain',null)">
-          Draggable Helmet
-        </div>
-        <div class="jacket-draggable" draggable="true" ondragstart="event.dataTransfer.setData('text/plain',null)">
-          Draggable Jacket
-        </div>
-        <div class="pants-draggable" draggable="true" ondragstart="event.dataTransfer.setData('text/plain',null)">
-          Draggable Pants
-        </div>
-        <div class="boots-draggable" draggable="true" ondragstart="event.dataTransfer.setData('text/plain',null)">
-          Draggable Boots
-        </div>
-      </div>
-
-
-      <main class="drop-zone">
-        <card class="dropzone">helmet goes here</card>  
-        <card class="dropzone">jacket goes here</card> 
-        <card class="dropzone">pants go here</card>  
-        <card class="dropzone">boots goes here</card>   
-      </main>
+  drag()
+  return (  
+    <>
+    <h1>Get the Fire Fighter Ready!</h1>
+    <div className="align-right"> 
+      <img src={firefighter_hat} alt="hat" height="180px" width="auto"/>
+      <img src={firefighter_pants} alt="pants" height="180px" width="auto" object-fit="contain"/>
+      <img src={firefighter_boots} alt="boots" height="50px" width="auto"/>
+      <img src={firefighter_jacket} alt="jacket" height="180px" width="auto"/>
     </div>
+
+      <main className="firefighter">
+        <card className="dropzone"></card>  
+        <card className="headspace"></card>
+        <card className="dropzone"></card> 
+        <card className="dropzone"></card>  
+        <card className="dropzone"></card>   
+      </main>
+    </>
   );
 }
 
